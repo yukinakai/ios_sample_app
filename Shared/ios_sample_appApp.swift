4f4a1f9f92843fa506a4ab8e3ebbffa08ad6ec36
yukinakai <10libra20.y@gmail.com>
2021-10-08 21:50:04 +0900

    Initial Commit

---
+++ Shared/ios_sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sample_appApp.swift
+//  Shared
+//
+//  Created by yuki nakai on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct ios_sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
